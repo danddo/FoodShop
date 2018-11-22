49221b3bd82e3aa8518bf30bedb796ae07251c52
danddo <danieldoron2@gmail.com>
2018-11-22 20:16:36 +0200

    most of the components are sturctured

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test-Recipe",'this is a test',"https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg"),
+    new Recipe("Test-Recipe2",'this is a test2',"https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg")
+  ];
 
   constructor() { }
 
